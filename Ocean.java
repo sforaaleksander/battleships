fd6cc7e3f678c8804a337eb7a692d3ac16f9edf9
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-03-23 13:21:59 +0100

    game board printing with labels

--- Ocean.java
+++ Ocean.java
@@ -1,9 +1,10 @@
-class Ocean{
+import java.util.Arrays;
+
+class Ocean {
     private int oceanSize;
     private Square[][] theOcean;
 
-
-    Ocean(int oceanSize){
+    Ocean(int oceanSize) {
         this.oceanSize = oceanSize;
         theOcean = new Square[oceanSize][oceanSize];
         for (int i = 0; i < oceanSize; i++) {
@@ -13,36 +14,38 @@ class Ocean{
         }
     }
 
-
-    public Square[][] getTable(){
+    public Square[][] getTable() {
         return this.theOcean;
     }
 
-    public void placeOnTable(Ship newShip){
-        if (newShip.getOrientation().equals("HORIZONTAL")){
-        for (int i=newShip.getPosX(); i < newShip.getPosX() + newShip.getLength(); i ++){
-            theOcean[newShip.getPosY()][i].mark();
-        }
-    } else{
-        for (int i = newShip.getPosY(); i < newShip.getPosY() + newShip.getLength(); i ++){
-            theOcean[i][newShip.getPosX()].mark();
-        }
+    public void placeOnTable(Ship newShip) {
+        if (newShip.getOrientation().equals("HORIZONTAL")) {
+            for (int i = newShip.getPosX(); i < newShip.getPosX() + newShip.getLength(); i++) {
+                theOcean[newShip.getPosY()][i].mark();
+            }
+        } else {
+            for (int i = newShip.getPosY(); i < newShip.getPosY() + newShip.getLength(); i++) {
+                theOcean[i][newShip.getPosX()].mark();
+            }
 
         }
     }
-    
 
-
-    public String toString(){
-        String output = "";
-        for (int i=0; i<this.oceanSize; i++){
-            for (int j=0; j<this.oceanSize; j++){
+    public String toString() {
+        String output = " ";
+        String[] letters = new String[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+        for (int x = 1; x < 11; x++) {
+            output += " " +x;
+        }
+        output += "\n";
+        for (int i = 0; i < this.oceanSize; i++) {
+            output += letters[i];
+            for (int j = 0; j < this.oceanSize; j++) {
                 output += theOcean[i][j].toString();
             }
             output += "\n";
         }
-    return output;
+        return output;
     }
 
-    
 }
